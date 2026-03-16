4d89c0c15b16d58d782aed861d6cfb7e5f414dd0
ariffaysal <ariffaysal001@gmai.com>
2026-03-16 13:42:31 +0600

    mm

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android and ios",
+                        name = "Android and ios only ",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
